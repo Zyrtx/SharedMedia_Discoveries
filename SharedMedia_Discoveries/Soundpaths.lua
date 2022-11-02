c52c2c69d99c8168a80c5ef70acea1b159256582
Zyrtx <61031875+Zyrtx@users.noreply.github.com>
2022-11-02 11:28:24 +0100

    initial commit

---
+++ SharedMedia_Discoveries/Soundpaths.lua
@@ -0,0 +1,25 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cff3CE13FStorm|r", [[Interface\Addons\SharedMedia_Discoveries\sound\Storm.ogg]])
+LSM:Register("sound", "|cff3CE13FBusiness - Fuck|r", [[Interface\Addons\SharedMedia_Discoveries\sound\FAK.ogg]])
+LSM:Register("sound", "|cff3CE13FBusiness - Maximum Retard|r", [[Interface\Addons\SharedMedia_Discoveries\sound\MAXIMUMRETARD.ogg]])
+LSM:Register("sound", "|cff3CE13FBusiness - Godverdomme|r", [[Interface\Addons\SharedMedia_Discoveries\sound\GODVERDOMME.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
